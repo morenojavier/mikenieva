25abfb80badfcecd1911263f5f35dc9836b3e522
Miguel Nieva <miguelnieva@conexionew.com>
2016-04-23 16:47:48 -0500

    Prueba final

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ~/webapps/miguelnieva/ghost/content/themes/mikenieva
 git pull central master
-cd ~/webapps/miguelnieva/ghost/bin
-stop && start
+cd ~/webapps/miguelnieva/ghost
+bin/stop && bin/start
